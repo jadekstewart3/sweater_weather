690fcd7332696171fbc738ac1132794e6a480238
Jade Stewart <jadekstewart3@gmail.com>
2023-04-22 20:04:48 -0600

    feat: create user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  # before_validation :downcase_email
+
+  validates :email, uniqueness: true,
+                    presence: true,
+                    format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates_presence_of :password
+
+  # private
+  # def downcase_email
+  #   self.email = email.downcase if email.present?
+  # end
+end
