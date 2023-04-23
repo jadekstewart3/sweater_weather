ccd6821c60832698bc70d142bcc4fd4937efe293
Jade Stewart <jadekstewart3@gmail.com>
2023-04-22 20:03:54 -0600

    test: test user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end
+
+  it "should validate that it's a properly formatted email" do
+    poppy = User.new(email: "popy_rocky_road@gmail.com", password: "password")
+    expect(poppy.valid?).to be(true)
+  
+    poppy = User.new(email: "fasd324223", password: "password")
+    expect(poppy.valid?).to be(false)
+  end
+end
