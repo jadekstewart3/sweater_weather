016e77dd03c031b7a13a9b72520319e09b4ebfb5
Jade Stewart <jadekstewart3@gmail.com>
2023-04-22 20:08:53 -0600

    test: re add ignoring case sensitivity line, decided it would be best to downcase email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
 
-    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
   end
