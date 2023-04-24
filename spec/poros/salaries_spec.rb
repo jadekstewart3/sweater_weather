b873c7aac55cc9de9d6c3aff84877af43b82dc71
Jade Stewart <jadekstewart3@gmail.com>
2023-04-24 11:43:46 -0600

    Add additional layer of testing to salaries_spec

--- spec/poros/salaries_spec.rb
+++ spec/poros/salaries_spec.rb
@@ -13,8 +13,25 @@ RSpec.describe Salaries do
     it "has attributes" do 
       expect(@salaries.id).to eq(nil)
       expect(@salaries.destination).to eq("denver")
+
+      expect(@salaries.forecast).to be_a(Hash)  
+      expect(@salaries.forecast).to have_key("summary")
+      expect(@salaries.forecast["summary"]).to be_a(String)
+      
+      expect(@salaries.forecast).to have_key("temperature")
+      expect(@salaries.forecast["temperature"]).to be_a(String)
+      
       expect(@salaries.salaries).to be_an(Array)
-      expect(@salaries.forecast).to be_a(Hash)
+      @salaries.salaries.each do |job|
+        expect(job).to have_key("title")
+        expect(job["title"]).to be_a(String)
+        expect(job).to have_key("min")
+        expect(job["min"]).to be_a(String)
+        expect(job).to have_key("max")
+        expect(job["max"]).to be_a(String)
+      end
+
+      
     end
   end
 end
