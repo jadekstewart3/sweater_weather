b1b031cd1a10a710c8a87dab40f6a30f989d007b
Jade Stewart <jadekstewart3@gmail.com>
2023-04-23 20:40:41 -0600

    fix: change before action when setting api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation :downcase_email
+  before_create :set_api_key
 
   validates :email, uniqueness: true,
                     presence: true,
                     format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_presence_of :password
+  validates :password_confirmation, presence: true
+  validates :api_key, uniqueness: true
 
   private
   def downcase_email
     self.email = email.downcase if email.present?
+  end
+
+  def set_api_key
+    self.api_key = SecureRandom.base64.tr("+/=","Qrt")
   end
 end
