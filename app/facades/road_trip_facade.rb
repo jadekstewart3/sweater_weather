3311e6d2e9b669366deef7cfb2e43f5820e41da4
Jade Stewart <jadekstewart3@gmail.com>
2023-04-25 13:22:26 -0600

    feat: complete roadtrip facade and adjust to allow for error handling

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,11 +1,20 @@
 class RoadTripFacade
-  
   def get_trip_info(origin, destination)
     route_data = RouteService.new.get_route(origin, destination)
-    coordinates = GeocodeService.new.get_coordinates(destination)
-    forecast = WeatherService.new.forecast(coordinates)
-    RoadTrip.new(route_data, forecast)
-  end
 
-  
+    if route_data[:info][:statuscode] == 0
+      coordinates = GeocodeService.new.get_coordinates(destination)
+      forecast = WeatherService.new.forecast(coordinates)
+      RoadTrip.new(route_data, forecast)
+    else
+      impossible_route_data = {
+                    id: nil,
+                    start_city: origin,
+                    end_city: destination,
+                    travel_time: "impossible",
+                    weather_at_eta: {}
+                  }
+      ImpossibleTrip.new(impossible_route_data)
+    end
+  end
 end
